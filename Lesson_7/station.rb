b5d7aa122e11f345a4ec314692f9ce1528f9e17a
aleksposte <aleks.poste@gmail.com>
2016-09-20 13:33:38 +0600

    Adding methods loads in the class carriage

---
+++ Lesson_7/station.rb
@@ -0,0 +1,69 @@
+
+class Station
+
+include InstanceCounter
+include Validate
+
+
+NAME_STATION = /^[a-zа-я\d]/i
+
+@@all = []
+
+attr_accessor :name, 
+              :trains,
+              :cargo_train,
+              :passenger_train
+
+  def initialize(name)
+    @@all.push(self)
+    @name = name
+    validate!
+    @trains = {
+                :cargo     => [],
+                :passenger => []
+    }
+  end
+
+  def add_train(train)
+    @trains[train.type].push(train.num)
+    puts "На станцию #{name} Прибыл поезд #{train.num}"
+  end
+
+  def delete_train(train)
+    @trains[train.type].delete(train.num)
+    puts "Со станции #{name} отбыл поезд #{train.num}"
+  end
+
+  def print_all_trains
+    puts "На станции #{name} находятся грузовые поезда:"
+      @trains[:cargo].each do |n|
+        puts "#{n}"
+      end
+    puts "На станции #{name} находятся пассажирские поезда:"
+      @trains[:passenger].each do |n|
+        puts "#{n}"
+      end
+  end 
+
+  def self.all
+    @@all
+  end
+
+# У класса Station:
+# написать метод, который принимает блок и проходит по всем поездам на станции, 
+# передавая каждый поезд в блок.
+
+  def trains_in(&block)
+    block = @trains.each { |train| }
+  end
+
+  private
+
+  # validate_station
+  def validate!
+    # raise "Наименование станции не может быть пустым" if name.nil?
+    raise puts "Наименование станции не соответствует формату" if @name !~ NAME_STATION
+    raise puts "Длина наименования станции слишком велика" if @name.length > 5
+  end
+
+end
---
+++ Lesson_7/var.rb
@@ -0,0 +1,91 @@
+
+
+# Добавить атрибут общего кол-ва мест (задается при создании вагона)
+# Добавить метод, который "занимает места" в вагоне (по одному за раз)
+# Добавить метод, который возвращает кол-во занятых мест в вагоне
+
+
+# class Carriage
+  
+#   attr_accessor :type
+
+#   # @seats
+    
+#   def initialize(capacity)
+#    @capacity = capacity
+#    @load = 0
+#   end
+
+# # Добавить метод, который "занимает места" в вагоне (по одному за раз)
+#   def load
+#     if @load < @capacity 
+#       @load += 1
+#       puts "Вы заняли место, свободных осталось #{@capacity - @load}"
+#     else
+#       puts "Мест больше нет"
+#     end
+#   end
+
+# # Добавить метод, который возвращает кол-во занятых мест в вагоне
+#   def occupied
+#     @load
+#   end
+
+# # Добавить метод, возвращающий кол-во свободных мест в вагоне.
+#   def vacancies
+#     @capacity - @load
+#   end
+
+# end
+
+
+# class CargoCarriage
+  
+#   def initialize(capacity)
+#    @type = :cargo
+#    @capacity = capacity
+#    @load = 0
+#   end
+
+# # Добавить метод, которые "занимает объем" в вагоне (объем указывается в качестве параметра метода)
+#   def load(value)
+#     if value < @capacity 
+#       @load += value
+#       puts "Вы заняли объем #{value}, свободно осталось #{@capacity - @load}"
+#     else
+#       puts "Места больше нет"
+#     end
+#   end
+
+# # Добавить метод, который возвращает занятый объем
+#   def occupied
+#     @load
+#   end
+
+# # Добавить метод, который возвращает оставшийся (доступный) объем
+#   def vacancies
+#     @capacity - @load
+#   end
+
+# end
+
+# У класса Station:
+# написать метод, который принимает блок и проходит по всем поездам на станции, 
+# передавая каждый поезд в блок.
+
+
+
+
+
+
+# c = CargoCarriage.new(5)
+# c.load(2)
+# puts c.occupied
+# puts c.vacancies
+
+# c.load(4)
+# puts c.occupied
+# puts c.vacancies
+
+
+
